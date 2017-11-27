ad483c45699255bccfc080ba67d137e1f083fd50
Stefan Majer <stefan.majer@f-i-ts.de>
2017-11-27 17:05:10 +0100

    output plugin is configured in configfile

--- start.sh
+++ start.sh
@@ -6,5 +6,4 @@ ls -l /fluent-bit/bin/
 
 /fluent-bit/bin/fluent-bit -c /fluent-bit/etc/fluent-bit.conf \
                            -e /fluent-bit/bin/out_redis.so \
-                           -o redis \
                            -i cpu
