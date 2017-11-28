29870076bd58c5ee6c994705b7f91596b43c8921
Stefan Majer <stefan.majer@f-i-ts.de>
2017-11-28 13:13:30 +0100

    do not default to a cpu input

--- start.sh
+++ start.sh
@@ -2,8 +2,5 @@
 
 set -e
 
-ls -l /fluent-bit/bin/
-
 /fluent-bit/bin/fluent-bit -c /fluent-bit/etc/fluent-bit.conf \
-                           -e /fluent-bit/bin/out_redis.so \
-                           -i cpu
+                           -e /fluent-bit/bin/out_redis.so
