59fdf8b378755dfc65ffbb67a9fa56f1fa5ac1ad
Stefan Majer <stefan.majer@f-i-ts.de>
2017-11-30 10:01:25 +0100

    More tests

---
+++ out_redis_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateJSON(t *testing.T) {
+	record := make(map[interface{}]interface{})
+	record["key"] = "value"
+	record["five"] = 5
+	js, err := createJSON("2006-01-02 15:04:05.999999999 -0700 MST", "atag", record)
+
+	if err != nil {
+		assert.Fail(t, "it is not expected that the call to createJSON fails:%v", err)
+	}
+	assert.NotNil(t, js, "json must not be nil")
+	result := make(map[string]interface{})
+	err = json.Unmarshal(js, &result)
+	if err != nil {
+		assert.Fail(t, "it is not expected that unmarshal of json fails:%v", err)
+	}
+	assert.Equal(t, result["@timestamp"], "2006-01-02T22:04:05.999999999Z")
+	assert.Equal(t, result["@tag"], "atag")
+	assert.Equal(t, result["key"], "value")
+	assert.Equal(t, result["five"], float64(5))
+}
